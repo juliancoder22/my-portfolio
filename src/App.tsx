0651c82e9448351f353afca0fa3281138f9ad6cf
Julian <julian.andres.aquino33@gmail.com>
2024-04-18 14:04:26 -0300

    About component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { AboutComponent } from "./components/AboutComponent/AboutComponent"
 import HomeComponent from "./components/HomeComponent/HomeComponent"
+import SkillsComponent from "./components/SkillsComponent/SkillsComponent"
 
 function App() {
 
   return (
     <>
     <HomeComponent />
+    <SkillsComponent />
+    <AboutComponent />
     </>
   )
 }
